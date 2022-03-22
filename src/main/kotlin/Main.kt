9caa1a27efd786041c6abb37d17047dddd0b0444
stevenwhy <stevenyildirim@gmail.com>
2022-03-21 20:34:15 -0400

    insert new interval and merge any overlapping

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import kotlin.math.max
+import kotlin.math.min
 
 fun main(args: Array<String>) {
 
@@ -23,25 +24,54 @@ fun main(args: Array<String>) {
     println("Expected: false , actual: ${doesOverlap(listOfPairs)}")
 
     println("Merge Intervals #2.1 --------------------------------------")
-    listOfPairs = listOf(Pair(1,3), Pair(5,7), Pair(8,12))
-    println("Merge Intervals #1.2")
+    var listOfIntervals = listOf(Interval(1,3), Interval(5,7), Interval(8,12))
     println("[[1,3], [5,7], [8,12]], New Interval=[4,6]")
-    println("Expected: [[1,3], [4,7], [8,12]] , actual: ${addInterval(listOfPairs, Pair(4,6))}")
-    listOfPairs = listOf(Pair(1,3), Pair(5,7), Pair(8,12))
+    println("Expected: [[1,3], [4,7], [8,12]] , actual: ${addInterval(listOfIntervals.toMutableList(), Interval(4,6))}")
+    listOfIntervals = listOf(Interval(1,3), Interval(5,7), Interval(8,12))
     println("[[1,3], [5,7], [8,12]], New Interval=[4,10]")
-    println("Expected: [[1,3], [4,12]] , actual: ${addInterval(listOfPairs, Pair(4,10))}")
-    listOfPairs = listOf(Pair(2,3), Pair(5,7))
+    println("Expected: [[1,3], [4,12]] , actual: ${addInterval(listOfIntervals.toMutableList(), Interval(4,10))}")
+    listOfIntervals = listOf(Interval(2,3), Interval(5,7))
     println("[[2,3],[5,7]], New Interval=[1,4]")
-    println("Expected: [[1,4], [5,7]] , actual: ${addInterval(listOfPairs, Pair(1,4))}")
+    println("Expected: [[1,4], [5,7]] , actual: ${addInterval(listOfIntervals.toMutableList(), Interval(1,4))}")
 }
 
 /*
     addInterval takes in a list of Pairs and a new Pair to add
         Each pair represents the start and end of an interval
-        All intervals given are assumed non-overlapping
+        All intervals given are assumed non-overlapping, sorted by start value
         Function adds the new Pair and merges where required to maintain mutual exclusivity
  */
+fun addInterval(intervals: MutableList<Interval>, newInterval: Interval): MutableList<Interval> {
 
+    var index = 0
+    while(index < intervals.size) {
+        val temp = intervals[index]
+        if(temp.second < newInterval.first) index++
+        else {
+            // there is an overlap
+            val start = min(temp.first, newInterval.first)
+            //now we need to find the end value
+            var end = max(temp.second, newInterval.second)
+            // but this can overlap with next value
+            val indexToUpdate = index
+            index++
+            while(index < intervals.size) {
+                if(end < intervals[index].first) break
+                else if (end <= intervals[index].second) {
+                    end = intervals[index].second
+                    intervals.removeAt(index)
+                    break
+                }
+                intervals.removeAt(index)
+            }
+            intervals[indexToUpdate].first = start
+            intervals[indexToUpdate].second = end
+            break
+        }
+    }
+    return intervals
+}
+data class Interval(var first: Int, var second: Int)
 /*
     mergeIntervals takes in a list of Pairs.
        Each pair represents the start and end of an interval
